b93eff530dcde9cb101307204777da2c5955ec57
victor muendo <135633599+metroliv@users.noreply.github.com>
2024-07-30 11:55:04 +0300

    Update models.py

--- Extracurricular/models.py
+++ Extracurricular/models.py
@@ -1,14 +1,21 @@
-# events/models.py
 from django.db import models
 from django.contrib.auth.models import User
 
 class Events(models.Model):
+    STATUS_CHOICES = [
+        ('upcoming', 'Upcoming'),
+        ('ongoing', 'Ongoing'),
+        ('completed', 'Completed'),
+    ]
+
     event_name = models.CharField(max_length=100, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     date = models.DateField(null=True, blank=True)
     time = models.TimeField(null=True, blank=True)
     location = models.CharField(max_length=255, null=True, blank=True)
     organizer = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='upcoming')
+    image = models.ImageField(upload_to='event_images/', blank=True, null=True)
 
     def __str__(self):
         return self.event_name
