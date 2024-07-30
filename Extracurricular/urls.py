4d8b78789a6e86fc10fe66ef66989a5862306345
victor muendo <135633599+metroliv@users.noreply.github.com>
2024-07-30 11:53:17 +0300

    Update urls.py

--- Extracurricular/urls.py
+++ Extracurricular/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('event_list/', views.event_list, name='event_list'),
     path('create/', views.event_create, name='event_create'),
+    path('<int:id>/', views.event_detail, name='event_detail'),
 ]
