1623b4000c664008ef68869537ba69915e3b4df5
victor muendo <135633599+metroliv@users.noreply.github.com>
2024-07-30 11:54:26 +0300

    Update forms.py

--- Extracurricular/forms.py
+++ Extracurricular/forms.py
@@ -1,8 +1,13 @@
 # events/forms.py
 from django import forms
 from .models import Events
+from django.contrib.auth.models import User
 
 class EventForm(forms.ModelForm):
     class Meta:
         model = Events
-        fields = ['event_name', 'description', 'date', 'time', 'location', 'organizer']
+        fields = ['event_name', 'description', 'date', 'time', 'location', 'organizer', 'status', 'image']
+        widgets = {
+            'date': forms.DateInput(attrs={'type': 'date'}),
+            'time': forms.TimeInput(attrs={'type': 'time'}),
+        }
